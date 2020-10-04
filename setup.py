a2b34fb96011bbdb132e50f6e2308c7bfed5a96e
americanthinker <americanthinker@gmail.com>
2020-10-04 09:14:29 -0600

    Starting project with base cookie-cutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict sentiment of airline tweets',
+    author='Chris Sanchez',
+    license='',
+)
